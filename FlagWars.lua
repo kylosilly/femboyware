5cf3a2ad377eca4f9920e5a6341d6dfdf221a6bc
kylosilly <gangggevil@gmail.com>
2024-10-04 22:52:13 +0200

    Update FlagWars.lua

--- FlagWars.lua
+++ FlagWars.lua
@@ -7,7 +7,9 @@ local GunMods = {
     RecoilMin = 0,
     RecoilMax = 0,
     ShotCooldown = 0,
-    BulletSpeed = 9999999
+    BulletSpeed = 9999999,
+    ReloadTime = 0,
+    ShotEffect = "Rocket"
 }
 
 local function modifyToolProperties(character)
@@ -15,10 +17,16 @@ local function modifyToolProperties(character)
         local holdingTool = character:FindFirstChildOfClass("Tool")
         if holdingTool then
             for propertyName, propertyValue in pairs(GunMods) do
-                local property = holdingTool:FindFirstChild(propertyName)
-                or (holdingTool:FindFirstChild("Configuration") and holdingTool.Configuration:FindFirstChild(propertyName))
+                local property = holdingTool:FindFirstChild(propertyName) 
+                 or (holdingTool:FindFirstChild("Configuration") 
+                 and holdingTool.Configuration:FindFirstChild(propertyName))
+
                 if property then
-                    property.Value = propertyValue
+                    if property:IsA("StringValue") then
+                        property.Value = tostring(propertyValue)
+                    else
+                        property.Value = propertyValue
+                    end
                 end
             end
         end
@@ -26,8 +34,12 @@ local function modifyToolProperties(character)
     end
 end
 
-player.CharacterAdded:Connect(modifyToolProperties)
+local function onCharacterAdded(character)
+    modifyToolProperties(character)
+end
+
+player.CharacterAdded:Connect(onCharacterAdded)
 
 if player.Character then
-    modifyToolProperties(player.Character)
+    onCharacterAdded(player.Character)
 end

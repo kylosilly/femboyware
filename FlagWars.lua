57de04b9cd3954ba356fbf8d01cd10feba293485
kylosilly <gangggevil@gmail.com>
2024-10-04 19:02:46 +0200

    Update FlagWars.lua

--- FlagWars.lua
+++ FlagWars.lua
@@ -1,22 +1,24 @@
 local player = game.Players.LocalPlayer
 
+local GunMods = {
+    CurrentAmmo = 7777,
+    MaxSpread = 0,
+    MinSpread = 0,
+    RecoilMin = 0,
+    RecoilMax = 0,
+    ShotCooldown = 0,
+    BulletSpeed = 9999999
+}
+
 local function modifyToolProperties(character)
     while true do
         local holdingTool = character:FindFirstChildOfClass("Tool")
         if holdingTool then
-            local currentAmmo = holdingTool:FindFirstChild("CurrentAmmo")
-            if currentAmmo then
-                currentAmmo.Value = 7777
-            end
-
-            local config = holdingTool:FindFirstChild("Configuration")
-            if config then
-                for _, v in ipairs(config:GetChildren()) do
-                    if v.Name == "MaxSpread" or v.Name == "MinSpread" or v.Name == "RecoilMin" or v.Name == "RecoilMax" or v.Name == "ShotCooldown" then
-                        v.Value = 0
-                    elseif v.Name == "BulletSpeed" then
-                        v.Value = 9999999
-                    end
+            for propertyName, propertyValue in pairs(GunMods) do
+                local property = holdingTool:FindFirstChild(propertyName)
+                or (holdingTool:FindFirstChild("Configuration") and holdingTool.Configuration:FindFirstChild(propertyName))
+                if property then
+                    property.Value = propertyValue
                 end
             end
         end
@@ -24,12 +26,8 @@ local function modifyToolProperties(character)
     end
 end
 
-local function onCharacterAdded(character)
-    modifyToolProperties(character)
-end
-
-player.CharacterAdded:Connect(onCharacterAdded)
+player.CharacterAdded:Connect(modifyToolProperties)
 
 if player.Character then
-    onCharacterAdded(player.Character)
+    modifyToolProperties(player.Character)
 end
